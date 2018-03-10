bcb62a9126774881d64cd0561a37879d1d63cd5b
matt pate <matt@sandboxhq.com>
2018-03-10 18:02:05 +0000

    create tax input component

---
+++ src/components/TaxInput/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class TaxInput extends Component {
+  render() {
+    return {
+
+      <div>tax input</div>
+    }
+  }
+}
+
+
+export default TaxInput;
