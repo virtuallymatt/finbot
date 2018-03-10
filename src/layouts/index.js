0cfc60e924b20a841e361f754de120e94f51c20b
matt pate <matt@sandboxhq.com>
2018-03-10 13:54:08 +0000

    import semantic css build globally in in index layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
 import './index.css'
+import '../../semantic/dist/semantic.min.css';
 
 const TemplateWrapper = ({ children }) => (
   <div>
